85c1eb183ef58103d7c5d0f5b9fbe83308caf869
Luis-Cortez <messibarbro@gmail.com>
2025-02-16 11:41:39 -0800

    title endpoint implemented

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,15 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function findBook( key, userValue) {
+  for( book in books ){
+    if( books[book][key].toLowerCase() == userValue ){
+        return books[book];
+    }
+    continue
+}
+  return false;
+}
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -46,17 +55,7 @@ public_users.get('/author/:author', async function (req, res) {
     const {author} = await req.params;
     const name = author.split("+").join(" ").toLowerCase();
 
-    function findAuthor() {
-      for( book in books ){
-        if( books[book].author.toLowerCase() == name ){
-            return books[book];
-        }
-        continue
-    }
-      return false;
-    }
-
-    const bookDetails = findAuthor();
+    const bookDetails = findBook("author", name);
 
     if( bookDetails ){
       return  res.status(200).json(bookDetails);
@@ -70,9 +69,23 @@ public_users.get('/author/:author', async function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const {title} = await req.params;
+    const text = title.split("+").join(" ").toLowerCase();
+
+    const bookDetails = findBook("title", text);
+
+    if( bookDetails ){
+      return  res.status(200).json(bookDetails);
+      }
+
+    return res.status(404).json(" Book by that title doesnt exist ");
+  } catch (error) {
+      console.log(error)
+      return res.status(500).json({message: "Server error"});
+  }
 });
 
 //  Get book review
